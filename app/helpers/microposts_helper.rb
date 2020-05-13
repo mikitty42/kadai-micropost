2d25cb3b11823ebd931968c912cf08c4a159ce4c
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-05-13 14:10:15 +0900

    確認画面を作成しました

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,9 @@
 module MicropostsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_microposts_path
+    elsif action_name == 'edit'
+      micropost_path
+    end
+  end
 end
