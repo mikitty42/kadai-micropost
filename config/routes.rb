9c95df235b7430dfac76a3d705389e5b0c3199b7
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-05-13 14:06:32 +0900

    ルーティングを作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :microposts do
+    collection do
+      post :confirm
 end
